ebe16aaf0277a27dba59c8ee1b11699f0d473739
Amartya Mishra <kushmishra8@gmail.com>
2020-07-28 03:41:32 +0530

    Updates

---
+++ precache-manifest.af47021c54a6127136c047e0954896b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfa74b9bf95ab494f00d4991f88b392",
+    "url": "/SMFSReact_app/index.html"
+  },
+  {
+    "revision": "873a8dba4d4a7d55b3e2",
+    "url": "/SMFSReact_app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f4c70ad7657b4be7869c",
+    "url": "/SMFSReact_app/static/css/main.4abbf57b.chunk.css"
+  },
+  {
+    "revision": "873a8dba4d4a7d55b3e2",
+    "url": "/SMFSReact_app/static/js/2.c2f85b3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SMFSReact_app/static/js/2.c2f85b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c70ad7657b4be7869c",
+    "url": "/SMFSReact_app/static/js/main.206084c2.chunk.js"
+  },
+  {
+    "revision": "fa9170c456c2adf8b607",
+    "url": "/SMFSReact_app/static/js/runtime-main.04492e3a.js"
+  }
+]);
